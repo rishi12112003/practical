a50f19d862414c5b1c8a4d31bd10edbef593ddc1
rishi12112003 <168420553+rishi12112003@users.noreply.github.com>
2024-04-29 15:11:17 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        
+        int sum = num1 + num2;
+        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
